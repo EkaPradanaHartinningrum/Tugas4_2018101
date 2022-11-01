60554ffcff9044b13d02e4af1977dd80a27b8d88
EkaPradanahartinningrum <eka4833@gmail.com>
2022-11-01 09:20:31 +0700

    2018101

--- app/src/main/java/com/example/tugas3_view/MainActivity.java
+++ app/src/main/java/com/example/tugas3_view/MainActivity.java
@@ -1,29 +1,75 @@
 package com.example.tugas3_view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
-import android.view.View;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mengganti actionbar dengan toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //memanggil drawer_layout dari activity_main.xml
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView =
+                findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //membuat hamburger icon pada toolbar dan animasinya
+        ActionBarDrawerToggle toggle = new
+                ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //membuat default navigation menu select
+        if(savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new presiden_fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_presiden);}
     }
+    //drawer menu fragment handler
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem
+                                                    item) {
+        switch (item.getItemId()){
+            case R.id.nav_presiden:
 
-    public void soeharto(View view) {
-        Intent a = new Intent(MainActivity.this, soeharto.class);
-        startActivity(a);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new presiden_fragment()).commit();
+                break;
+            case R.id.nav_provinsi:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new provinsi_fragment()).commit();
+                break;
+            case R.id.nav_bantuan:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new bantuan_fragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
-
-    public void jokowi(View view) {
-        Intent a = new Intent(MainActivity.this, jokowi.class);
-        startActivity(a);
-    }
-
-
-    public void MainActivity(View view) {
+    //on back press behavior
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
